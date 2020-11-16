8b3185761e837a2fd479add52fff913ece7a89d1
spandranki <sameera.pandranki7@gmail.com>
2020-11-16 03:59:48 -0500

    first commit

---
+++ gitpush.bat
@@ -0,0 +1,12 @@
+@echo off
+
+echo *************git status*************
+git status
+echo *************git add*************
+git add .
+echo *************git commit*************
+git commit -m "a bit"
+echo *************git push*************
+git push
+echo *************git status*************
+git status
